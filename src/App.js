c2924e52f8fc2bb6f36835fd8a08c47efebbb58d
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-11-17 20:57:59 -0300

    Analytics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css'
+// import './App.css'
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Analytics from './components/Analytics'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Analytics />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
       <div
         className={
           !nav
-            ? 'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+            ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
             : 'fixed left-[-100%]'
         }
       >
